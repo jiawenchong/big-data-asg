9e7e3f892ccace4f79bbf1b236aea1055b842659
jiawenchong <137248552+jiawenchong@users.noreply.github.com>
2023-07-26 12:51:37 +0800

    Update datarecord.java

--- code/datarecord.java
+++ code/datarecord.java
@@ -1,4 +1,7 @@
-private static class DataRecord {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRecord {
     double speed;
     double travelTime;
     String borough;
@@ -10,4 +13,17 @@ private static class DataRecord {
     }
 
     // Additional methods to convert to/from string representation (if required)
+    public static DataRecord fromString(String dataString) {
+        String[] fields = dataString.split(",");
+        double speed = Double.parseDouble(fields[0]);
+        double travelTime = Double.parseDouble(fields[1]);
+        String borough = fields[2];
+        return new DataRecord(speed, travelTime, borough);
+    }
+
+    // Implement this method based on your requirements or use any random sampling method
+    public static List<DataRecord> takeRandomSample(List<DataRecord> dataRecords, int sampleSize) {
+        // Your implementation here
+        return new ArrayList<>();
+    }
 }
