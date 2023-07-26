aff9697c93747869581328fd460c67088fcbd967
jiawenchong <137248552+jiawenchong@users.noreply.github.com>
2023-07-26 12:44:41 +0800

    Create datarecord.java

---
+++ code/datarecord.java
@@ -0,0 +1,13 @@
+private static class DataRecord {
+    double speed;
+    double travelTime;
+    String borough;
+
+    public DataRecord(double speed, double travelTime, String borough) {
+        this.speed = speed;
+        this.travelTime = travelTime;
+        this.borough = borough;
+    }
+
+    // Additional methods to convert to/from string representation (if required)
+}
